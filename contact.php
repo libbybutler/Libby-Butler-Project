e5ad5c2e67e70a91533240183c7c33eb600607dc
libbybutler <libbypoulton@hotmail.com>
2023-09-30 21:37:42 +0100

    update to php

--- contact.php
+++ contact.php
@@ -1,22 +1,23 @@
 <?php
-$name = $_Post['name'];
-$visitor_email = $_post['email'];
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
 $message = $_POST['message'];
 
 $email_from = 'LibbyButlerProject@outlook.com';
 
-$email_subject ='New Form Submission';
+$email_subject ="New Form Submission";
 
 $email_body ="User Name: $name./n".
-    "User Email: $visitor_email./n";
+    "User Email: $visitor_email./n".
+        "User Message: $message./n";
 
 $to = "LibbyButlerProject@outlook.com";
 
-$headers ="From" $email_from /r/n";
+$headers ="From" $email_from \r\n";
 
-$headers .="Reply-to: $visitor_email /r/n";
+$headers .= "Reply-To: $visitor_email \r\n";
 
-mail($to, $email_subject,$email_body,$headers);
+mail($to,$email_subject,$email_body,$headers);
 
 header("Location: contact.html");
 ?>
