55f4eb8935ff543d0c3f8ec4e125b6cf302ed569
libbybutler <libbypoulton@hotmail.com>
2023-10-01 02:29:28 +0100

    changes to php

--- contact.php
+++ contact.php
@@ -1,4 +1,24 @@
 <?php
+if(isset($_POST["submit"])) {
+$recipient = "libbybutlerproject@outlook.com"; //my email
+echo $subject = 'contact form from Libby Butler Project';
+echo $name = $_POST ["yourName"];
+echo $email = $_POST["yourEmail"];
+echo $phone = $_POST["yourPhone"];
+echo $location = $_POST["yourLocate"];
+echo  $message = $_POST["yourMessage"];
+
+ $mailBody="Name: $name\nEmail: $email\n\n$message"; 
+
+ mail($recipient, $subject, $mailBody, "From: $name <$email>");
+
+echo $thankYou="<p>Thank you! We will be in contact with you shortly.</p>";
+
+}
+?>
+
+
+<!-- <?php
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $message = $_POST['message'];
@@ -20,7 +40,7 @@ $headers .= "Reply-To: $visitor_email \r\n";
 mail($to,$email_subject,$email_body,$headers);
 
 header("Location: contact.html");
-?>
+?>  -->
 
 function IsInjected($str)
 {
@@ -51,3 +71,5 @@ if(IsInjected($visitor_email))
     echo "Bad email value!";
     exit;
 }
+
+
